331a148275657cc79c439f9f474f8cd132e9fefa
nathaliapinheiro <mesquita.natth@gmail.com>
2019-10-06 12:29:09 -0300

    adjust tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {InvestmentProvider} from './context/investment-context';
+import App from '../App';
+import {InvestmentProvider} from '../context/investment-context';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/__tests__/investmentForm.test.js
+++ src/__tests__/investmentForm.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
-import {InvestmentProvider} from '../../context/investment-context';
-import InvestmentForm from './index';
-import expectExport from 'expect';
+import {InvestmentProvider} from '../context/investment-context';
+import InvestmentForm from '../components/investmentForm/index';
+import '@testing-library/jest-dom/extend-expect';
 
 test('change values and send form', () => {
     const {getByTestId} = render(
@@ -19,11 +19,11 @@ test('change values and send form', () => {
     expect(typeSelect.textContent).toEqual('');
 
     fireEvent.change(valueInput, {target: {value: '100000'}});
-    fireEvent.change(dateInput, {target: {value: '05/10/2019'}});
+    fireEvent.change(dateInput, {target: {value: '2019-10-05'}});
     fireEvent.change(typeSelect, {target: {value: 'RENDA_FIXA'}});
 
     expect(valueInput.value).toEqual('1.000,00');
-    expect(dateInput.value).toEqual('05/10/2019');
+    expect(dateInput.value).toEqual('2019-10-05');
     expect(typeSelect.value).toEqual('RENDA_FIXA');
 
     fireEvent.click(addButton);
